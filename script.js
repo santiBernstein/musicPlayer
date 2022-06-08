0fce3c25f15b0fff90a189f8a788a735caae7c03
santiBernstein <santiago.bernstein@gmail.com>
2022-06-08 17:39:19 -0300

    btn prev and next js

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
+
+// music
+const songs = [
+    {
+        name: 'uno',
+        displayName: 'Demo 1',
+        artist: 'Jamiro',
+    },
+    {
+        name: 'dos',
+        displayName: 'Demo 2',
+        artist: 'Jamiro',
+    },
+    {
+        name: 'tres',
+        displayName: 'Demo 3',
+        artist: 'Jamiro',
+    },
+    {
+        name: 'cuatro',
+        displayName: 'Demo 4',
+        artist: 'Jamiro',
+    },
+];
 
 // check if playing
 let isPlaying = false;
@@ -24,3 +51,42 @@ function pauseSong(){
 
 // play or pause event listener
 playBtn.addEventListener('click', ()=> (isPlaying ? pauseSong() : playSong() ) );
+
+// update DOM
+function loadSong(song){
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src = `img/${song.name}.jpg`
+}
+
+// current song
+let songIndex = 0;
+
+// prev song
+function prevSong(){
+    songIndex--;
+    if(songIndex < 0){
+        songIndex = songs.length -1;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+
+// next song
+function nextSong(){
+    songIndex++;
+    if(songIndex > songs.length -1){
+        songIndex = 0; 
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// on load - select first song
+loadSong(songs[songIndex]);
+
+// event listeners
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
